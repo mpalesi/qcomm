5db168f6599f96142a1d84f9893688443683c3bb
Maurizio Palesi <maurizio.palesi@gmail.com>
2025-08-08 11:28:44 +0200

    Modified display format of teleportation_time_components to show the percentage fraction as comment

--- teleportation_time.cpp
+++ teleportation_time.cpp
@@ -18,6 +18,24 @@ void TeleportationTime::display(const string& prefix) const
   
   cout << prefix << "teleportation_time: " << total_time << " # sec " << endl;
 
+  cout << prefix << "teleportation_time_components:" << endl;
+
+  cout << prefix << prefix << "epr_pair_generation_time: " << t_epr << " # sec " 
+       << "(" << 100*t_epr/total_time << "%)" << endl;
+  
+  cout << prefix << prefix << "epr_pair_distribution_time: " << t_dist << " # sec "
+       << "(" << 100*t_dist/total_time << "%)" << endl;
+  
+  cout << prefix << prefix << "pre_processing_time: " << t_pre << " # sec "
+       << "(" << 100*t_pre/total_time << "%)" << endl;
+  
+  cout << prefix << prefix << "classical_transfer_time: " << t_clas << " # sec " 
+       << "(" << 100*t_clas/total_time << "%)" << endl;
+  
+  cout << prefix << prefix << "post_processing_time: " << t_post << " # sec "
+       << "(" << 100*t_post/total_time << "%)" << endl;
+  
+  /*
   cout << prefix << "teleportation_time_components:" << endl
        << prefix << prefix << "epr_pair_generation_time:" << endl
        << prefix << prefix << prefix << "time: " << t_epr << " # sec" << endl
@@ -34,6 +52,7 @@ void TeleportationTime::display(const string& prefix) const
        << prefix << prefix << "post_processing_time:" << endl
        << prefix << prefix << prefix << "time: " << t_post << " # sec" << endl
        << prefix << prefix << prefix << "percent: " << 100*t_post/total_time << " # %" << endl;
+  */
 }
 
 double TeleportationTime::getTotalTeleportationTime() const
